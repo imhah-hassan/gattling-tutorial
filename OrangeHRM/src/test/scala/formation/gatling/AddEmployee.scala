cf66f6233f5ca87786d3ba75e6077dc6c85c3247
Hassan <hassan.imhah@outlook.fr>
2021-11-21 18:32:06 +0100

    Lesson3 : feeders

--- OrangeHRM/src/test/scala/formation/gatling/AddEmployee.scala
+++ OrangeHRM/src/test/scala/formation/gatling/AddEmployee.scala
@@ -9,17 +9,23 @@ import scala.util.Random
 
 class AddEmployee extends Simulation {
 
-	val base_url = "https://opensource-demo.orangehrmlive.com/"
-	val admin_pwd = "admin123"
+	val base_url = "http://192.168.1.208:90"
+	val admin_pwd = "Hassan$2022"
+	
 	object Utils {
 		def randomCode( ) : String = {
 			val rnd = new Random()
 			return (10000 + rnd.nextInt(88888)).toString()
 		}
+		// Think time entre 1 et 3 s
+		def thinktime( ) : FiniteDuration = {
+			val rnd = new Random()
+			return (rnd.nextInt(10) + 20).milliseconds
+		}
 	}
 
 
-		val httpProtocol = http
+	val httpProtocol = http
 		.baseUrl( base_url )
 		// .proxy(Proxy("localhost", 8888))
 		.inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.woff2""", """.*\.(t|o)tf""", """.*\.png""", """.*detectportal\.firefox\.com.*"""), WhiteList())
@@ -28,64 +34,27 @@ class AddEmployee extends Simulation {
 		.acceptLanguageHeader("en-US,fr;q=0.8,fr-FR;q=0.5,en;q=0.3")
 		.userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0")
 
-	val headers_0 = Map(
-		"Cache-Control" -> "max-age=0",
-		"Sec-Fetch-Dest" -> "document",
-		"Sec-Fetch-Mode" -> "navigate",
-		"Sec-Fetch-Site" -> "none",
-		"Sec-Fetch-User" -> "?1",
-		"Upgrade-Insecure-Requests" -> "1")
-
-	val headers_1 = Map(
-		"Origin" ->  base_url ,
-		"Sec-Fetch-Dest" -> "document",
-		"Sec-Fetch-Mode" -> "navigate",
-		"Sec-Fetch-Site" -> "same-origin",
-		"Sec-Fetch-User" -> "?1",
-		"Upgrade-Insecure-Requests" -> "1")
-
-	val headers_2 = Map(
-		"Accept" -> "*/*",
-		"Sec-Fetch-Dest" -> "empty",
-		"Sec-Fetch-Mode" -> "cors",
-		"Sec-Fetch-Site" -> "same-origin",
-		"X-Requested-With" -> "XMLHttpRequest")
-
-	val headers_4 = Map(
-		"Sec-Fetch-Dest" -> "document",
-		"Sec-Fetch-Mode" -> "navigate",
-		"Sec-Fetch-Site" -> "same-origin",
-		"Sec-Fetch-User" -> "?1",
-		"Upgrade-Insecure-Requests" -> "1")
-
-	val headers_5 = Map(
-		"Accept" -> "application/json, text/javascript, */*; q=0.01",
-		"Sec-Fetch-Dest" -> "empty",
-		"Sec-Fetch-Mode" -> "cors",
-		"Sec-Fetch-Site" -> "same-origin",
-		"X-Requested-With" -> "XMLHttpRequest")
-
-	val headers_7 = Map(
-		"Content-Type" -> "multipart/form-data; boundary=---------------------------26091326749537329901312335116",
-		"Origin" ->  base_url ,
-		"Sec-Fetch-Dest" -> "document",
-		"Sec-Fetch-Mode" -> "navigate",
-		"Sec-Fetch-Site" -> "same-origin",
-		"Sec-Fetch-User" -> "?1",
-		"Upgrade-Insecure-Requests" -> "1")
-
-
+	val employees = separatedValues("employees.csv", ';').eager.random
 
 	val scn = scenario("AddEmployee")
 		// HOME
-		.exec(http("login")
+		.feed(employees)
+    .exec(session => {
+			println("Nom = " + session("nom").as[String])
+			println("Prenom = " + session("prenom").as[String])
+			println("Matricule = " + session("matricule").as[String])
+      println("Date de naissance = " + session("date_de_naissance").as[String])
+      session
+    })
+
+    .exec(http("login")
 			.get("/index.php/auth/login")
 			.check(status.is(200))
 			.check( css("#csrf_token","value").saveAs("csrf_token"))
 
 			//	.headers(headers_0)
 		)
-		.pause(0)
+		.pause(Utils.thinktime())
 
 		// LOGIN
 		.exec(http("validateCredentials")
@@ -110,13 +79,11 @@ class AddEmployee extends Simulation {
 
 				//	.headers(headers_2)
 		))
-		.pause(0)
+		.pause(Utils.thinktime())
 
 		// ADD EMPLOYEE
-		.exec (session => session.set("code", Utils.randomCode()))
 		.exec(http("viewPimModule")
 			.get("/index.php/pim/viewPimModule")
-			.check(status.is(200))
 			// .headers(headers_4)
 			.resources(http("getEmployeeListAjax")
 			.get("/index.php/pim/getEmployeeListAjax"),
@@ -129,23 +96,23 @@ class AddEmployee extends Simulation {
 
 				//.headers(headers_4)
 			))
-		.pause(0)
+		.pause(Utils.thinktime())
 		.exec(http("addEmployeePost")
 			.post("/index.php/pim/addEmployee")
-			.headers(headers_7)
+			.headers(Map("Content-Type" -> "multipart/form-data; boundary=---------------------------26091326749537329901312335116"))
 			.body(ElFileBody("formation/gatling/addemployee/0007_request.dat"))
 			.check(status.is(200))
+			.check(substring("Échec de Sauvegarde").notExists)
 			.check(css("#personal__csrf_token", "value").saveAs("personal_csrf_token"))
 			.check(css("#personal_txtEmpID", "value").saveAs("personal_txtEmpID"))
-
 			.resources(http("getHolidayAjax")
 			.get("/index.php/leave/getHolidayAjax?year=2021&_=1634564077374"),
 			//.headers(headers_5),
-            http("getWorkWeekAjax")
+			http("getWorkWeekAjax")
 			.get("/index.php/leave/getWorkWeekAjax?_=1634564077375")
 			//.headers(headers_5)
 			))
-		.pause(0)
+		.pause(Utils.thinktime())
 
 		// EMPLOYEE DATAILS
 		.exec(http("viewPersonalDetails")
@@ -153,10 +120,10 @@ class AddEmployee extends Simulation {
 			//.headers(headers_1)
 			.formParam("personal[_csrf_token]", "${personal_csrf_token}")
 			.formParam("personal[txtEmpID]", "${personal_txtEmpID}")
-			.formParam("personal[txtEmpFirstName]", "Performance${code}")
+			.formParam("personal[txtEmpFirstName]", "${prenom}")
 			.formParam("personal[txtEmpMiddleName]", "")
-			.formParam("personal[txtEmpLastName]", "Gatling${code}")
-			.formParam("personal[txtEmployeeId]", "${code}")
+			.formParam("personal[txtEmpLastName]", "${nom}")
+			.formParam("personal[txtEmployeeId]", "${matricule}")
 			.formParam("personal[txtOtherID]", "")
 			.formParam("personal[txtLicenNo]", "")
 			.formParam("personal[txtLicExpDate]", "yyyy-mm-dd")
@@ -165,10 +132,9 @@ class AddEmployee extends Simulation {
 			.formParam("personal[optGender]", "1")
 			.formParam("personal[cmbMarital]", "Married")
 			.formParam("personal[cmbNation]", "64")
-			.formParam("personal[DOB]", "1989-11-25")
+			.formParam("personal[DOB]", "${date_de_naissance}")
 			.formParam("personal[txtEmpNickName]", "")
 			.formParam("personal[txtMilitarySer]", "")
-			.check(status.is(200))
 			.resources(http("getWorkWeekAjax")
 			.get("/index.php/leave/getWorkWeekAjax?_=1634564107742"),
 			//.headers(headers_5),
@@ -176,7 +142,7 @@ class AddEmployee extends Simulation {
 			.get("/index.php/leave/getHolidayAjax?year=2021&_=1634564107741")
 			//.headers(headers_5)
 			))
-		.pause(0)
+		.pause(Utils.thinktime())
 
 
 		// SEARCH EMPLOYEE
@@ -185,19 +151,19 @@ class AddEmployee extends Simulation {
 			.check(status.is(200))
 			.check(css("#defaultList__csrf_token", "value").saveAs("defaultList_csrf_token"))
 			.check(css("#empsearch__csrf_token", "value").saveAs("empsearch_csrf_token"))
-			.check(css("#resultTable>tbody>tr:nth-of-type(1)>td:nth-of-type(1)>input[type='checkbox']", "value").saveAs("empId"))
-			.check(css("#resultTable>tbody>tr:nth-of-type(1)>td:nth-of-type(2)>a").saveAs("empNumber"))
+			.check(css("#resultTable>tbody>tr:nth-of-type(2)>td:nth-of-type(1)>input[type='checkbox']", "value").saveAs("empId"))
+			.check(css("#resultTable>tbody>tr:nth-of-type(2)>td:nth-of-type(2)>a").saveAs("empNumber"))
 
 			//.headers(headers_4)
 			.resources(http("getEmployeeListAjax")
 			.get("/index.php/pim/getEmployeeListAjax")
 			//.headers(headers_5)
 			))
-		.pause(8)
+		.pause(Utils.thinktime())
 		.exec(http("viewEmployeeList")
 			.post("/index.php/pim/viewEmployeeList")
 			//.headers(headers_1)
-			.formParam("empsearch[employee_name][empName]", "Gatling")
+			.formParam("empsearch[employee_name][empName]", "")
 			.formParam("empsearch[employee_name][empId]", "${empId}")
 			.formParam("empsearch[id]", "")
 			.formParam("empsearch[employee_status]", "0")
@@ -209,26 +175,24 @@ class AddEmployee extends Simulation {
 			.formParam("empsearch[_csrf_token]", "${empsearch_csrf_token}")
 			.formParam("pageNo", "")
 			.formParam("hdnAction", "search")
-			.check(status.is(200))
 			.resources(http("getEmployeeListAjax")
 			.get("/index.php/pim/getEmployeeListAjax")
 			//.headers(headers_5)
 			))
-		.pause(0)
+		.pause(Utils.thinktime())
 
 		// DELETE EMPLOYEE
-		.exec(http("deleteEmployees")
-			.post("/index.php/pim/deleteEmployees")
-			//.headers(headers_1)
-			.formParam("defaultList[_csrf_token]", "${defaultList_csrf_token}")
-			.formParam("chkSelectAll", "")
-			.formParam("chkSelectRow[]", "${empId}")
-			.check(status.is(200))
-			.resources(http("getEmployeeListAjax")
-			.get("/index.php/pim/getEmployeeListAjax")
-			//.headers(headers_5)
-			))
-		.pause(0)
+//		.exec(http("deleteEmployees")
+//			.post("/index.php/pim/deleteEmployees")
+//			//.headers(headers_1)
+//			.formParam("defaultList[_csrf_token]", "${defaultList_csrf_token}")
+//			.formParam("chkSelectAll", "")
+//			.formParam("chkSelectRow[]", "${empId}")
+//			.resources(http("getEmployeeListAjax")
+//			.get("/index.php/pim/getEmployeeListAjax")
+//			//.headers(headers_5)
+//			))
+//		.pause(Utils.thinktime())
 		
 		// LOGOUT
 		.exec(http("logout")
